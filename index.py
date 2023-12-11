a6759a59771172fde5ac79e44d8ff2f9a2388c55
ivan dobry <ivandobry@hotmail.es>
2023-12-11 13:24:01 +0100

    rename constants

--- index.py
+++ index.py
@@ -1,25 +1,25 @@
-# importing required modules
+"""Module providing a function printing pdf file"""
 import PyPDF2
 
 # creating pdf path
-pdfIle = 'nomina.pdf'
- 
+PDF_PATH = 'nomina.pdf'
+
 # creating a pdf file object
-pdfFileObj = open(pdfIle, 'rb')
- 
-# creating a pdf reader object
+pdfFileObj = open(PDF_PATH, 'rb')
+
+# creating a pdf reader objectW
 pdfReader = PyPDF2.PdfReader(pdfFileObj)
- 
+
 # printing number of pages in pdf file
 print(len(pdfReader.pages))
 
-pageNumber = 1
- 
+PAGE_NUMBER = 1
+
 # extracting text from pages
 for page in pdfReader.pages:
-    print('PAGE: ', pageNumber)
+    print('PAGE: ', PAGE_NUMBER)
     print(page.extract_text())
-    pageNumber += 1
-    
+    PAGE_NUMBER += 1
+
 # closing the pdf file object
 pdfFileObj.close()
