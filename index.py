3f35cce3ed349f3c06f569c7193520724f70ef89
ivan dobry <ivandobry@hotmail.es>
2023-12-11 11:11:27 +0100

    print page number

--- index.py
+++ index.py
@@ -1,18 +1,25 @@
 # importing required modules
 import PyPDF2
+
+# creating pdf path
+pdfIle = 'nomina.pdf'
  
 # creating a pdf file object
-pdfFileObj = open('nomina.pdf', 'rb')
+pdfFileObj = open(pdfIle, 'rb')
  
 # creating a pdf reader object
 pdfReader = PyPDF2.PdfReader(pdfFileObj)
  
 # printing number of pages in pdf file
 print(len(pdfReader.pages))
+
+pageNumber = 1
  
 # extracting text from pages
 for page in pdfReader.pages:
+    print('PAGE: ', pageNumber)
     print(page.extract_text())
+    pageNumber += 1
     
 # closing the pdf file object
 pdfFileObj.close()
