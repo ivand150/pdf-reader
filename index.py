27564c9980266acf18c3658fd21b2f256b9193a3
ivan dobry <ivandobry@hotmail.es>
2023-12-11 10:27:04 +0100

    remove comment

--- index.py
+++ index.py
@@ -10,14 +10,9 @@ pdfReader = PyPDF2.PdfReader(pdfFileObj)
 # printing number of pages in pdf file
 print(len(pdfReader.pages))
  
-# creating a page object
-pageObj = pdfReader.pages[0]
-
+# extracting text from pages
 for page in pdfReader.pages:
     print(page.extract_text())
     
- 
-# extracting text from page
- 
 # closing the pdf file object
 pdfFileObj.close()
